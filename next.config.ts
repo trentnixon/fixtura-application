2c8eefc37403171d43a35adc5d56be747574e4d8
trentnixon <trentnixon@gmail.com>
2026-04-01 21:08:43 +1100

    Update Sentry configuration and dependencies. Change project name in Sentry settings, upgrade @sentry/nextjs to version 10.47.0, and enable sending user PII. Refactor webpack settings for automatic instrumentation of Vercel Cron Monitors and tree-shaking options. Adjust logging behavior in next.config.ts and add Sentry logging in example API route.

--- next.config.ts
+++ next.config.ts
@@ -27,10 +27,10 @@ export default withSentryConfig(nextConfig, {
 
   org: "na-g0d",
 
-  project: "fixtura-marketing",
+  project: "fixtura-application",
 
   // Only print logs for uploading source maps in CI
-  silent: !process.env["CI"],
+  silent: !process.env.CI,
 
   // For all available options, see:
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
@@ -44,12 +44,17 @@ export default withSentryConfig(nextConfig, {
   // side errors will fail.
   // tunnelRoute: "/monitoring",
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  webpack: {
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+    // Tree-shaking options for reducing bundle size
+    treeshake: {
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      removeDebugLogging: true,
+    },
+  },
 });
